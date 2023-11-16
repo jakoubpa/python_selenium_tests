0fd3aa9fe4c789cb29153220578dc85c9628a722
PavelJakoube <jakoubep@seznam.cz>
2023-11-16 18:11:15 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import unittest
+from selenium import webdriver
+import page
+
+class PythonOrgSearch(unittest.TestCase):
+    
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+        self.driver.get("https://www.python.org")
+        
+    def test_search_python(self):
+        mainPage = page.MainPage(self.driver)
+        assert mainPage.is_title_matching()
+        mainPage.search_text_element = "pycon"
+        mainPage.click_go_button()
+        search_result_page = page.SearchResultPage(self.driver)
+        assert search_result_page.is_results_found()
+
+        
+    def tearDown(self):
+        self.driver.close()
+        
+if __name__ == "__main__":
+    unittest.main()
